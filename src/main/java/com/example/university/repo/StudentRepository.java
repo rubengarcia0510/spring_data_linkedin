90784710d1b4c7bdc58c9f3b5bdaa4a8387fc0cd
Ruben Garcia <rubengarcia0510@gmail.com>
2022-11-02 18:12:15 -0300

    update

--- src/main/java/com/example/university/repo/StudentRepository.java
+++ src/main/java/com/example/university/repo/StudentRepository.java
@@ -17,51 +17,16 @@ public interface StudentRepository extends CrudRepository<Student, Integer>{
 
     List<Student> findByFullTime(boolean fullTime);
     List<Student> findByAge(Integer age);
-    List<Student> findByAttendeLastName(String last);
-    Student findByAttendeFirstNameAndAttendeLastName(String firstName,String lastName);
-    Student findByAttende(Person person);
+    List<Student> findByAttendeeLastName(String last);
+    Student findByAttendeeFirstNameAndAttendeeLastName(String firstName,String lastName);
+    Student findByAttendee(Person person);
+    List<Student> findByAgeGreatherThan(int minimumAge);
+    List<Student> findByAgeLessThan(int maximumAge);
+    List<Student> findByAttendeeLastNameIgnoreCase(String lastName);
+    List<Student> findByAttendeeLastNameLike(String lastName);
+    Student findFirstByOrderByAttendeeLastNameAsc();
+    Student findTopByOrderByAgeDesc();
+    List<Student> findTop3ByOrderByAgeDesc();
 
-    default <S extends Student> S save(S s) {
-        return null;
-    }
 
-    default <S extends Student> Iterable<S> saveAll(Iterable<S> iterable) {
-        return null;
-    }
-
-    default Optional<Student> findById(Integer integer) {
-        return Optional.empty();
-    }
-
-    default boolean existsById(Integer integer) {
-        return false;
-    }
-
-    default Iterable<Student> findAll() {
-        return null;
-    }
-
-    default Iterable<Student> findAllById(Iterable<Integer> iterable) {
-        return null;
-    }
-
-    default long count() {
-        return 0;
-    }
-
-    default void deleteById(Integer integer) {
-
-    }
-
-    default void delete(Student student) {
-
-    }
-
-    default void deleteAll(Iterable<? extends Student> iterable) {
-
-    }
-
-    default void deleteAll() {
-
-    }
 }
